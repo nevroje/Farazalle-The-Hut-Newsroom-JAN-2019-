7c21761819e326528d54efd14fcdb63b87816a2c
nevroje <joakim.berglind@gmail.com>
2019-03-28 19:48:29 +0100

    accepted merges from alecia/upstream

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_03_28_091428) do
-ActiveRecord::Schema.define(version: 2019_03_24_204610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
